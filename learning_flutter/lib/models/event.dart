c34cdf2058264e8cb3c3c194bed7b465ae660daf
Pavel <olkov1634520@mail.ru>
2022-04-13 13:58:58 +0300

    lab5

---
+++ learning_flutter/lib/models/event.dart
@@ -0,0 +1,9 @@
+class Event{
+  late String name;
+  late String localiton;
+ late DateTime StartDateTime;
+
+ Event({required this.name,required this.localiton, required this.StartDateTime});
+
+}
+
